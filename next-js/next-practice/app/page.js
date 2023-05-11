2fc12818988869eaeceadb2b74cf692b272911bf
sunkeydokey <sunkeydokey@gmail.com>
2023-05-11 09:43:41 +0900

    nextjs 초기세팅

---
+++ next-js/next-practice/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import styles from './page.module.css';
+import { MongoClient } from 'mongodb';
+
+const Home = () => {
+  // const client = await MongoClient.connect('DB접속URL~~', { useNewUrlParser: true })
+  // const db = client.db("forum")
+  return <div>hi</div>;
+};
+
+export default Home;
